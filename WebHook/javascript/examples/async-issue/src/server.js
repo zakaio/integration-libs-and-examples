302420df99895e8963e4e0b11cf79633ff18cd14
Alexey Hodkov <alexey.hodkov@gmail.com>
2022-04-01 14:51:18 +0300

    fix

--- WebHook/javascript/examples/async-issue/src/server.js
+++ WebHook/javascript/examples/async-issue/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const crypto = require('crypto');
 const router = express.Router();
 const axios = require('axios');
 
