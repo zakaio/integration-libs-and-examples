1b4d8c4b370315f78a75e4ebce3e803fee89079d
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-06-14 11:58:46 +0300

    prepare to example

--- SSIOAuth/RFC6749/javascript/example/ecosystem.config.js
+++ SSIOAuth/RFC6749/javascript/example/ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps : [{
-    name: 'second-auth-demo',
+    name: 'proofspace-oauth-rfc6749-example',
     script: 'src/server.js',
     env: {
       PORT: '4607',
-      CLIENT_ID: 'ab95234d-e1ca-4fa1-862c-da239dd89b68',
-      CLIENT_SECRET: '2323r31r',
+      CLIENT_ID: '', // Insert client id here
+      CLIENT_SECRET: '', // Insert client secret here
       OAUTH_URL: 'https://test.proofspace.id/oauth2',
-      REDIRECT_URI: 'https://test.proofspace.id/auth-demo2/redirect',
-      ROOT_URI: '/',
-      JWT_KEY_FILE: "publicKey.pem",
-      JWT_ALGO: "RS256",
+      REDIRECT_URI: '', // Insert redirect uri here
+      ROOT_URI: '/',  // ROOT URI of webn application
+      JWT_KEY_FILE: "publicKey.pem", // file with public key
+      JWT_ALGO: "RS256", // one of  "RS256", "RS512", "SHARED_SECRET"
     },
   }]
 };
